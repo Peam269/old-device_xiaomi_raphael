ee9817f7693a3b3053dc14fb51847caadd741d95
Emil <emilwagenplast@gmail.com>
2022-08-02 13:30:30 +0200

    SparkOS bringup

--- spark_raphael.mk
+++ spark_raphael.mk
@@ -15,13 +15,13 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_raphael
+PRODUCT_NAME := spark_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9T Pro
@@ -32,13 +32,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Spark
+SPARK_BUILD_TYPE := UNOFFICIAL
+WITH_GAPPS := true
+WITH_FOD_ANIMATIONS := true
+TARGET_USES_BLUR := true
 EXTRA_UDFPS_ANIMATIONS := true
-
-# Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
-
-# Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
-# Derp
-DERP_BUILDTYPE := Official
-EXTRA_UDFPS_ANIMATIONS := true
